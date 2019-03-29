db248029f25e233d129c3d0be37c0e5cb18e920d
raylandar <raylandar@users.noreply.github.com>
2019-03-29 08:47:09 -0400

    Update ratings.setup.php

--- plugins/ratings/ratings.setup.php
+++ plugins/ratings/ratings.setup.php
@@ -5,8 +5,8 @@ Code=ratings
 Name=Star Ratings
 Category=community-social
 Description=Ratings plugin with jQuery stars
-Version=1.1.17
-Date=2015-03-07
+Version=1.1.18
+Date=2017-11-22
 Author=Cotonti Team
 Copyright=Copyright (c) Cotonti Team 2008-2016
 Notes=BSD License
@@ -16,13 +16,11 @@ Auth_members=RW
 Lock_members=
 Recommends_modules=page
 [END_COT_EXT]
-
 [BEGIN_COT_EXT_CONFIG]
 ratings_allowchange=01:radio::0:Allow users to change their ratings
 css=99:radio::1:Enable plugin CSS
 [END_COT_EXT_CONFIG]
 ==================== */
-
 /**
  * Ratings plugin setup file
  *
@@ -30,5 +28,4 @@ css=99:radio::1:Enable plugin CSS
  * @copyright (c) Cotonti Team
  * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
  */
-
 defined('COT_CODE') or die('Wrong URL');
