b260023f0107eb5e350aaed2d1042ed46f787381
raylandar <raylandar@users.noreply.github.com>
2019-03-29 08:46:17 -0400

    Update ratings.admin.php

--- plugins/ratings/ratings.admin.php
+++ plugins/ratings/ratings.admin.php
@@ -4,7 +4,6 @@
 Hooks=tools
 [END_COT_EXT]
 ==================== */
-
 /**
  * Administration panel - Manager of ratings
  *
@@ -12,45 +11,39 @@ Hooks=tools
  * @copyright (c) Cotonti Team
  * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
  */
-
 (defined('COT_CODE') && defined('COT_ADMIN')) or die('Wrong URL.');
-
 list($usr['auth_read'], $usr['auth_write'], $usr['isadmin']) = cot_auth('plug', 'ratings');
 cot_block($usr['isadmin']);
-
 require_once cot_incfile('ratings', 'plug');
-
 $t = new XTemplate(cot_tplfile('ratings.admin', 'plug', true));
-
 $adminhelp = $L['adm_help_ratings'];
 $adminsubtitle = $L['Ratings'];
-
 $id = cot_import('id','G','TXT');
 $maxperpage = ($cfg['maxrowsperpage'] && is_numeric($cfg['maxrowsperpage']) && $cfg['maxrowsperpage'] > 0) ? $cfg['maxrowsperpage'] : 15;
 list($pg, $d, $durl) = cot_import_pagenav('d', $maxperpage);
-
 /* === Hook  === */
 foreach (cot_getextplugins('admin.ratings.first') as $pl)
 {
 	include $pl;
 }
 /* ===== */
-
 if($a == 'delete')
 {
 	cot_check_xg();
 	$db->delete($db_ratings, 'rating_code = ' . $db->quote($id));
 	$db->delete($db_rated, 'rated_code = ' . $db->quote($id));
-
+	/* === Hook  === */
+	foreach (cot_getextplugins('admin.ratings.delete.done') as $pl)
+	{
+		include $pl;
+	}
+	/* ===== */
+	
 	cot_message('adm_ratings_already_del');
 }
-
-
 $totalitems = $db->countRows($db_ratings);
 $pagenav = cot_pagenav('admin', 'm=other&p=ratings', $d, $totalitems, $maxperpage, 'd', '', $cfg['jquery'] && $cfg['turnajax']);
-
 $sql = $db->query("SELECT * FROM $db_ratings WHERE 1 ORDER by rating_id DESC LIMIT $d, ".$maxperpage);
-
 $ii = 0;
 $jj = 0;
 /* === Hook - Part1 : Set === */
@@ -61,20 +54,17 @@ foreach ($sql->fetchAll() as $row)
 	$id2 = $row['rating_code'];
 	$sql1 = $db->query("SELECT COUNT(*) FROM $db_rated WHERE rated_code=" . $db->quote($id2));
 	$votes = $sql1->fetchColumn();
-
-	$rat_type = mb_substr($row['rating_code'], 0, 1);
-	$rat_value = mb_substr($row['rating_code'], 1);
-
+	$rat_type = $row['rating_area'];
+	$rat_value = $row['rating_code'];
 	switch($rat_type)
 	{
-		case 'p':
+		case 'page':
 			$rat_url = cot_url('page', 'id='.$rat_value);
 		break;
 		default:
 			$rat_url = '';
 		break;
 	}
-
 	$t->assign(array(
 		'ADMIN_RATINGS_ROW_URL_DEL' => cot_url('admin', 'm=other&p=ratings&a=delete&id='.$row['rating_code'].'&d='.$durl.'&'.cot_xg()),
 		'ADMIN_RATINGS_ROW_RATING_CODE' => $row['rating_code'],
@@ -96,7 +86,6 @@ foreach ($sql->fetchAll() as $row)
 	$ii++;
 	$jj = $jj + $votes;
 }
-
 $t->assign(array(
 	'ADMIN_RATINGS_URL_CONFIG' => cot_url('admin', 'm=config&n=edit&o=plug&p=ratings'),
 	'ADMIN_RATINGS_PAGINATION_PREV' => $pagenav['prev'],
@@ -106,16 +95,13 @@ $t->assign(array(
 	'ADMIN_RATINGS_ON_PAGE' => $ii,
 	'ADMIN_RATINGS_TOTALVOTES' => $jj
 ));
-
 cot_display_messages($t);
-
 /* === Hook  === */
 foreach (cot_getextplugins('admin.ratings.tags') as $pl)
 {
 	include $pl;
 }
 /* ===== */
-
 $t->parse('MAIN');
 if (COT_AJAX)
 {
